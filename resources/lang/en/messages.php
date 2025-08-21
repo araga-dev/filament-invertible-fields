25b5f017f9bd83d8a04e41b881778a8094af25a9
Mário Aragão <70496020+araga-dev@users.noreply.github.com>
2025-08-21 20:33:25 -0300

    Small Fixes

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,8 +5,4 @@ return [
     'except' => 'Except',
     'toggle_to_only' => 'Switch to: Only',
     'toggle_to_except' => 'Switch to: Except',
-
-    // Common labels used in README examples
-    'status' => 'Status',
-    'sku' => 'SKU',
 ];
--- resources/lang/pt_BR/messages.php
+++ resources/lang/pt_BR/messages.php
@@ -4,9 +4,5 @@ return [
     'only' => 'Somente',
     'except' => 'Exceto',
     'toggle_to_only' => 'Mudar para: Somente',
-    'toggle_to_except' => 'Mudar para: Exceto',
-
-    // Common labels used in README examples
-    'status' => 'Status',
-    'sku' => 'SKU',
+    'toggle_to_except' => 'Mudar para: Exceto'
 ];
